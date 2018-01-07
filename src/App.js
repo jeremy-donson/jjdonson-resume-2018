27771937c258f7aac1c24dfdc651b7c4edb360a7
Jeremy Donson <jjdonson@gmail.com>
2018-01-06 19:27:47 -0500

    Need to fix some things, but it is happening right.  Need heading, logo and modernizr.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Collapse, Navbar, Form } from 'reactstrap';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
 
-class App extends Component {
+class Example extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = { collapse: false };
+  }
+
+  toggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Button id="1" color="primary" onClick={this.toggle} style={{ borderColor: 'Black', marginBottom: '0rem' }}>TechExecOnline iPhone + iPad Apps</Button>
+        <Collapse isOpen={this.state.collapse}>
+          <Card>
+            <CardBody>
+            <ol>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://v4-alpha.getbootstrap.com/components/collapse/">Separate Actions</a></li>
+            <li>Border Font</li>
+            <li>Button Color</li>
+            <li>Button Border</li>
+            <li>Border Panels</li>
+            <li>Full Width Panel</li>
+            <li></li>
+            </ol>
+            </CardBody>
+          </Card>
+        </Collapse><br/>
+
+        <Button id="2" color="primary" onClick={this.toggle} style={{ borderColor: 'Black', marginBottom: '0rem' }}>TechExecOnline Android App</Button>
+        <Collapse isOpen={this.state.collapse}>
+          <Card>
+            <CardBody>
+            Anim pariatur cliche reprehenderit,
+             enim eiusmod high life accusamus terry richardson ad squid. Nihil
+             anim keffiyeh helvetica, craft beer labore wes anderson cred
+             nesciunt sapiente ea proident.
+            </CardBody>
+          </Card>
+        </Collapse>
+
+        <Button id="3" color="primary" onClick={this.toggle} style={{ borderColor: 'Black', marginBottom: '0rem' }}>TechExecOnline Support Services</Button>
+        <Collapse isOpen={this.state.collapse}>
+          <Card>
+            <CardBody>
+            Anim pariatur cliche reprehenderit,
+             enim eiusmod high life accusamus terry richardson ad squid. Nihil
+             anim keffiyeh helvetica, craft beer labore wes anderson cred
+             nesciunt sapiente ea proident.
+            </CardBody>
+          </Card>
+        </Collapse>
+
+        <Button id="4" color="primary" onClick={this.toggle} style={{ borderColor: 'Black', marginBottom: '0rem' }}>Roadmaps: webApp + phoneApps + webSite</Button>
+        <Collapse isOpen={this.state.collapse}>
+          <Card>
+            <CardBody>
+            Anim pariatur cliche reprehenderit,
+             enim eiusmod high life accusamus terry richardson ad squid. Nihil
+             anim keffiyeh helvetica, craft beer labore wes anderson cred
+             nesciunt sapiente ea proident.
+            </CardBody>
+          </Card>
+        </Collapse>
+
       </div>
     );
   }
 }
 
-export default App;
+export default Example;
+
+//  fontFamily: 'Arial', 
+// import { Collapse, Button, CardBody, Card, Progress } from 'reactstrap';
